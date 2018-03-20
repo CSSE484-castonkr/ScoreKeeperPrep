efcf3c2dd64a33a7d55c793f0842d51fd07aedfe
Kiana Caston <castonkr@rose-hulman.edu>
2018-03-20 12:05:08 -0400

    Initial Commit

---
+++ ScoreKeeperPrep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScoreKeeperPrep
+//
+//  Created by Kiana Caston on 3/20/18.
+//  Copyright © 2018 Kiana Caston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScoreKeeperPrep/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScoreKeeperPrep
+//
+//  Created by Kiana Caston on 3/20/18.
+//  Copyright © 2018 Kiana Caston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
